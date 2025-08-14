6ea84566a4fdaf1bb93f7e1dd0dc408d165e28ca
Wolf Pops <jawolfson3@gmail.com>
2025-08-14 09:15:05 -0700

    Create test_inference.py
    created tests folder with basic flow to force the return of text when this code runs

---
+++ tests/test_inference.py
@@ -0,0 +1,11 @@
+# test_inference.py
+# Placeholder test for inference.py
+
+from src import inference
+
+def test_handler():
+    result = inference.handler({})
+    print(result)
+
+if __name__ == "__main__":
+    test_handler()
