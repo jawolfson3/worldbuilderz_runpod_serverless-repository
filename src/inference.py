fd1afc70b0c5b70c82470a69e41b620c3e473689
Wolf Pops <jawolfson3@gmail.com>
2025-08-14 09:01:23 -0700

    Create inference.py
    added src folder and included dummy py file

---
+++ src/inference.py
@@ -0,0 +1,6 @@
+# inference.py
+# This file will contain the code that runs Mistral on RunPod
+
+def handler(event):
+    # For now, just return a placeholder message
+    return {"message": "Hello from Mistral RunPod endpoint!"}
